2ea554a6ce1c05c2ffdc19cb047b814cac6f4592
H Shafee Hamath <shafeehamath24@gmail.com>
2026-05-28 21:04:21 +0530

    Added Low Feedback Alerts SQL exercise

---
+++ Upskilling/Module-2/Module-2-ANSI-SQL-Using-MySQL/07_LowFeedbackAlerts.sql
@@ -0,0 +1,22 @@
+/* 07. Low Feedback Alerts
+   List all users who gave feedback
+   with a rating less than 3. */
+
+USE event_management;
+
+SELECT
+
+    u.full_name,
+    e.title,
+    f.rating,
+    f.comments
+
+FROM Feedback f
+
+JOIN Users u
+    ON f.user_id = u.user_id
+
+JOIN Events e
+    ON f.event_id = e.event_id
+
+WHERE f.rating < 3;
