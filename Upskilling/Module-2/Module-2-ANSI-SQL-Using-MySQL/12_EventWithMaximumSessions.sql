d7422cea4ad5c86d35a6f1cdd6df742d5bd159b9
H Shafee Hamath <shafeehamath24@gmail.com>
2026-05-28 21:36:17 +0530

    Added Event With Maximum Sessions SQL exercise

---
+++ Upskilling/Module-2/Module-2-ANSI-SQL-Using-MySQL/12_EventWithMaximumSessions.sql
@@ -0,0 +1,38 @@
+/* 12. Event with Maximum Sessions
+   List the event(s) with the highest
+   number of sessions. */
+
+USE event_management;
+
+SELECT
+
+    e.event_id,
+    e.title,
+    COUNT(s.session_id)
+    AS total_sessions
+
+FROM Events e
+
+JOIN Sessions s
+    ON e.event_id = s.event_id
+
+GROUP BY
+    e.event_id,
+    e.title
+
+HAVING COUNT(s.session_id) = (
+
+    SELECT MAX(session_count)
+
+    FROM (
+
+        SELECT COUNT(session_id)
+        AS session_count
+
+        FROM Sessions
+
+        GROUP BY event_id
+
+    ) AS temp
+
+);
